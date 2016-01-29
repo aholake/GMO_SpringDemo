0d261a2cc9d5f8da6601b30720688cec80cf6295
LocVT <nluit.tanloc@gmail.com>
2016-01-29 11:56:37 +0700

    Autowire Type Demo

--- src/main/java/example/autowwiring/App.java
+++ src/main/java/example/autowwiring/App.java
@@ -5,8 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
+		//Autowire by annotation
 		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Car car = (Car) context.getBean("carBean");
 		System.out.println(car);
+		
+		//autowire by xml configuration
+		ApplicationContext context1 = new ClassPathXmlApplicationContext("wiringbeans.xml");
+		Student student = (Student) context1.getBean("student");
+		System.out.println(student);
 	}
 }
---
+++ src/main/java/example/autowwiring/School.java
@@ -0,0 +1,14 @@
+package example.autowwiring;
+
+public class School {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
